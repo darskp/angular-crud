ffca6109d8175c7707d93beca09d1bcfa271f2bf
darskp <darshanpatgar100@gmail.com>
2024-12-22 18:32:32 +0530

    notes updated

--- notes.tsx
+++ notes.tsx
@@ -1,13 +1,46 @@
+//! 1. Overview
 // ng v - to check whether angular cli or not
+// How angular render by default?
 
 // ng new projectname
-
+// what is single page application - one index html and based on the route the component will execute and render the component reference to
 // angular.json -> configuration
 // 1 index.html -> directive -- <app-root></app-root> html tag
 
-//main.ts -- app-comp reference
+//main.ts -- app-comp reference and also we have app.config
 
-//component - part of the ui
+//! 2. route and navigation
+// app.routes.ts
+// [{
+//     *   path: 'parent',
+//     *   component: ParentComponent,
+//     *   children: [{
+//      *     path: 'list',
+//         *     component: ListComponent
+//      *   }, {
+//         *     path: 'child',
+//         *     component: ChildComponent
+//      *   }]
+//     * }]
+
+// add directive inside the app - <router-outlet></router-outlet> - to render the component
+//     < a routerLink = "/parent" ></ > and import in the component RouterLink
+
+// why navigation ? - angular is a spa and to achieve multiple page navigation we need to relay on the routing library.
+
+//! routes
+
+// routes
+// router - outlet - responsible for rendering the routes
+// router-link- to navigate
+// router link active
+
+//{path:"", component:name}
+// in .app html -- <router-outlet></router-outlet>
+// navigation
+// next - in anchor instead of ahref - routerLink import RouterOutlet
+
+//! 3. component - part of the ui
 //cli cmd -  ng generate component name
 
 //standalone - independent component
@@ -22,13 +55,22 @@
 // event binding - // <button (click) ="showMessage()">test</button>
 
 // data binding -- 2 types
+//how we bind the data to the html elements
 
 //? 1. one way
 //  1. .ts to .html(template)
-//     1. property binding - html element assiging value to the property - or attribute (classname)
+//     1. property binding - in html element assiging value to the property - or attribute (classname) and make dynamic
+            //  <input [value]="myName" type="text"/>
 //     2. interpolation - {{}}
 //  2.html(template) to .ts
 //     1.event binding - click, change, mouseenter, mouseleave,
+
+{/* <select (change) = "selectedValue($event)" >
+  <option value="">select</option>
+  <option value="t1">t1</option>
+  <option value="t2">t2</option>
+  <option value="t3">t3</option>
+</select > */}
 
 //? 2. two way
 //    ng model - 2 way, when you type any letters inside the input box same typed var values will be reflcted, whereever you have used them inside the component
@@ -47,46 +89,11 @@
 // ngIf, ngFor || ngClass, ngStyle
 // http module is deprecated
 
-// services -  where we include all our resuable code or in other words we can say
+// services -  where we include all our resuable code or in other words we can say 
 // used to write api calls related function, - angular cli  ng g s master
 
-//environments
-
-//ng generate environments
-// define inside the developent.ts
-// access using template literals
 
 
-//! routes
-
-// routes
-// router - outlet - responsible for rendering the routes
-// router-link- to navigate
-// router link active
-
-// {
-// path : '/user',
-// redirectTo: 'login',
-// pathMatch:'full' || 'prefix' || default,
-// children:[
-
-// ]
-// }
-
-// full: Exact match (entire URL path must match).
-// prefix: Match if the beginning of the URL matches the path.
-// default: Same as prefix (typically used in older Angular versions).
-
-//{path:"", component:name}
-// in .app html -- <router-outlet></router-outlet>
-// navigation
-// next - in anchor instead of ahref - routerLink import RouterOutlet
-
-// lets say after login u want to redirect to - dashaboard
-// router=inject(router);
-
-// inside login successfull fn = 
-// this.router.navigateByUrl('dashaboard');
 
 // control flow Statement
 // @if, @for,@switch - *ngIf and *ngFor (these control flow directives have been replaced with @if blocks as there is to no need ot import CommonModule, )
--- src/app/components/testd/testd.component.ts
+++ src/app/components/testd/testd.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-testd',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './testd.component.html',
   styleUrl: './testd.component.css'
 })
 export class TestdComponent {
+myName:string="Darshan";
+userName:string=""
+isBtnHidden:boolean=false;
+numbers:any[]=[1,2,3,4,5]
+
+  handleOnclick(){
+    alert("hi")
+  }
+  handleClearUsername(){
+    this.userName=""
+  }
+  handlebtnOnclick(){
+    this.isBtnHidden=!this.isBtnHidden;
+  }
 
 }
